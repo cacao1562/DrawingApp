04ad6170ff9cbb8df45c2be996d31983a827e38f
cacao <cacao1562@naver.com>
2021-03-07 00:25:09 +0900

    Add canvavs scale, Add save photo

--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 import PencilKit
+import PhotosUI
 
 class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
     
     @IBOutlet weak var btnCamera: UIBarButtonItem!
-    @IBOutlet weak var btnPencil: UIBarButtonItem!
     @IBOutlet weak var canvasView: PKCanvasView!
     
-    let canvasWidth: CGFloat = 760
+    let canvasWidth: CGFloat = 768
     let canvvasOverScrollHeight: CGFloat = 500
     
     var drawing = PKDrawing()
     var toolPicker: PKToolPicker!
+    var pencilFingerBarButtonItem: UIBarButtonItem!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -26,7 +27,7 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
         canvasView.delegate = self
         canvasView.drawing = drawing
         canvasView.alwaysBounceVertical = true
-        canvasView.drawingPolicy = .anyInput
+//        canvasView.drawingPolicy = .anyInput
         
         if #available(iOS 14.0, *) {
             toolPicker = PKToolPicker()
@@ -42,15 +43,92 @@ class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserv
         toolPicker.addObserver(self)
         canvasView.becomeFirstResponder()
         
+        
+        // Before iOS 14, add a button to toggle finger drawing.
+        if #available(iOS 14.0, *) { } else {
+            pencilFingerBarButtonItem = UIBarButtonItem(title: "Enable Finger Drawing",
+                                                        style: .plain,
+                                                        target: self,
+                                                        action: #selector(toggleFingerPencilDrawing))
+            navigationItem.rightBarButtonItems?.append(pencilFingerBarButtonItem)
+            canvasView.allowsFingerDrawing = false
+        }
+        
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let canvasScale = canvasView.bounds.width / canvasWidth
+        canvasView.minimumZoomScale = canvasScale
+        canvasView.maximumZoomScale = canvasScale
+        canvasView.zoomScale = canvasScale
+        updateContentSizeForDrawing()
+        canvasView.contentOffset = CGPoint(x: 0, y: -canvasView.adjustedContentInset.top)
+    }
+    
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        updateContentSizeForDrawing()
+    }
+    
     /// Hide the home indicator, as it will affect latency.
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
     }
+    
+    
+    func updateContentSizeForDrawing() {
+        let drawing = canvasView.drawing
+        let contentHeight: CGFloat
+        
+        if !drawing.bounds.isNull {
+            contentHeight = max(canvasView.bounds.height, (drawing.bounds.maxY + self.canvvasOverScrollHeight) * canvasView.zoomScale)
+        }else {
+            contentHeight = canvasView.bounds.height
+        }
+        
+        canvasView.contentSize = CGSize(width: canvasWidth * canvasView.zoomScale, height: contentHeight)
+    }
+
+    
 
     @IBAction func saveToAlbum(_ sender: Any) {
+        UIGraphicsBeginImageContextWithOptions(canvasView.bounds.size, false, UIScreen.main.scale)
+        canvasView.drawHierarchy(in: canvasView.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        if image != nil {
+            PHPhotoLibrary.shared().performChanges {
+                PHAssetChangeRequest.creationRequestForAsset(from: image!)
+            } completionHandler: { (success, error) in
+                if (success) {
+                    self.showAlert(msg: "사진첩에 저장 되었습니다.")
+                }else {
+                    self.showAlert(msg: "저장 실패 하였습니다.")
+                }
+            }
+
+        }
     }
+    
+    func showAlert(msg: String) {
+        let alert = UIAlertController(title: "", message: msg, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(confirm)
+        DispatchQueue.main.sync {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @objc
+    func toggleFingerPencilDrawing() {
+        if #available(iOS 14.0, *) { } else {
+            canvasView.allowsFingerDrawing.toggle()
+            let title = canvasView.allowsFingerDrawing ? "Disable Finger Drawing" : "Enable Finger Drawing"
+            pencilFingerBarButtonItem.title = title
+        }
+    }
+    
     
 }
 
